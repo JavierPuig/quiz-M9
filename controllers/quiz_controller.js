03b1c6f9c3dfd83f1895f3241eb11c51b7d4b152
JavierPuig <fcojavier.puig@gmail.com>
2015-06-18 14:05:14 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,11 +18,11 @@ exports.index = function(req, res){
 	console.log("busqueda: "+req.query.search);
 	if (req.query.search === undefined) {
 		models.Quiz.findAll().then(function(quizes){
-			res.render('quizes/index', { quizes: quizes });
+			res.render('quizes/index', { quizes: quizes, errors: []});
 		}).catch(function(error) { next(error); });
 	} else {
 		models.Quiz.findAll({where: ["pregunta like ?", "%"+req.query.search.replace("+","%")+"%"]}).then(function(quizes){
-		res.render('quizes/index', { quizes: quizes });
+		res.render('quizes/index', { quizes: quizes, errors: []});
 		}).catch(function(error) { next(error); });
 	}	
 };
@@ -31,7 +31,7 @@ exports.index = function(req, res){
 // GET /quizes/:id
 exports.show = function(req, res){
 	models.Quiz.find(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', { quiz: req.quiz });
+		res.render('quizes/show', { quiz: req.quiz, errors: []});
 	})
 };
 
@@ -41,7 +41,7 @@ exports.answer = function(req, res){
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado = 'Correcto';
 	}
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 
@@ -50,15 +50,20 @@ exports.new = function(req, res) {
 	var quiz = models.Quiz.build (
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req,res){
 	var quiz = models.Quiz.build( req.body.quiz );
-	console.log("Para crear pregunta")
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		console.log("Redireccionamos")
-		res.redirect('/quizes');
-	})
+	quiz.validate().then(function(err){
+		if (err) {
+			console.log("Error en crear pregunta");
+			res.render('quizes/new', {quiz: quiz, errors: err.errors});
+		} else {
+			quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+			console.log("OK en crear pregunta");
+			res.redirect('/quizes')})  // Redirección a lista de preguntas
+		}
+	});
 };
