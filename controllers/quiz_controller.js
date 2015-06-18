55e2c94891c298aa83af1278836a7ef393a1908e
JavierPuig <fcojavier.puig@gmail.com>
2015-06-18 22:51:51 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,7 +10,7 @@ exports.load = function(req, res, next, quizId){
 			} else { next(new Error('No existe quizId=' + quizId));}
 		}
 	).catch(function(error){next(error);});
-}
+};
 
 
 // GET /quizes
@@ -66,4 +66,26 @@ exports.create = function(req,res){
 			res.redirect('/quizes')})  // Redirección a lista de preguntas
 		}
 	});
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req,res){
+	var quiz = req.quiz;
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(function(err){
+		if (err){
+			res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		} else {
+			req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+			res.redirect('/quizes');});
+		}
+	}
+  );
 };
